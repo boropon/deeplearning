fc64d753b529f1365ac223fde4a3fbe1662eb9e6
miyagi <r23107007@jp.ricoh.com>
2018-02-25 13:11:38 +0900

    Fixed ftp.py bugs.

--- srcnn/src/ftp.py
+++ srcnn/src/ftp.py
@@ -1,9 +1,7 @@
-#!/usr/bin/python3
-
 import sys
 from ftplib import FTP
 
-def ftp_put(ip, filename):
+def ftp_put(ip, filename, superoption):
     #FTP login
     _ftp = FTP(ip)
     _ftp.login()
@@ -12,7 +10,7 @@ def ftp_put(ip, filename):
     _file = open(filename, 'rb')
 
     #FTP put
-    _ftp.storbinary("STOR " + filename, _file)
+    _ftp.storbinary('STOR ' + filename + ' ,' + superoption, _file)
 
     #ファイルクローズ
     _file.close()
@@ -20,12 +18,12 @@ def ftp_put(ip, filename):
     #FTP quit
     _ftp.quit()
 
-def send_to_mptiff(ip, filename)
+def send_to_mptiff(ip, filename):
     #スーパーオプション filetype=mediaprinttiff指定付きでFTP putを行う
-    ftp_put(ip, filename + ' ,filetype=mediaprinttiff')
+    ftp_put(ip, filename, 'filetype=mediaprinttiff')
 
 if __name__ == '__main__':
     args = sys.argv
-    ip = argv[1]
-    filename = argv[2]
+    ip = args[1]
+    filename = args[2]
     send_to_mptiff(ip, filename)
